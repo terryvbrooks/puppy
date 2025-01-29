8632563c7863f92388bb140563f079688ccb1939
Terry Brooks <terry_v_brooks@hotmail.com>
2025-01-29 20:20:40 +0000

    forth

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/terryvbrooks/puppy
 
 go 1.23.4
+
+require github.com/terryvbrooks/dog v0.0.0-20250129201409-a568f91b54aa // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/terryvbrooks/dog"
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "woof! woof! woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
